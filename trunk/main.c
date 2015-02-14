a5c15293fcee9c31feb55fb8b1728393418a9516
mtj@mtjones.com <mtj@mtjones.com@1654c09f-b0c2-b8de-f9d7-50004636501b>
2015-02-14 21:01:39 +0000

    Updates.

--- trunk/main.c
+++ trunk/main.c
@@ -5,20 +5,21 @@
 int main ( int argc, char *argv[] )
 {
    int i;
-   thread_t *sample;
-   instruction_t program[ 5 ] = { INSTR( INCX, 0 ), INSTR( INCY, 0 ),
-                                  INSTR( INCX, 0 ), INSTR( INCY, 0 ),
-                                  INSTR( JUMP, 0 ) };
+   thread_t *avatar_thread;
 
-   sample = thread_create( );
-   sample->load( sample, Avatar, program, 5, 0, 0 );
+   instruction_t aprog[ 5 ] = { INSTR_FMT1( INCX ), INSTR_FMT3( BXNE, 2, 0 ),
+                                INSTR_FMT1( DECX ), INSTR_FMT3( BXNE, 0, 2 ),
+                                INSTR_FMT2( JUMP, 0 ) };
+
+   avatar_thread = thread_create( );
+   avatar_thread->load( avatar_thread, Avatar, aprog, sizeof( aprog ), 0, 0 );
 
    for ( i = 0 ; i < 50 ; i++ )
    {
       printf("PC: %4d, CYC: %6d, X: %4d, Y: %4d\n",
-               sample->registers.pc, sample->registers.cyc, 
-               sample->registers.x,  sample->registers.y );
-      sample->step( sample );
+               avatar_thread->registers.pc, avatar_thread->registers.cyc, 
+               avatar_thread->registers.x,  avatar_thread->registers.y );
+      avatar_thread->step( avatar_thread );
    }
 
    return;
